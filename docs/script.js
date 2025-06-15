9095eee3f5a83d2581104ee3196a2336100857f5
Pandora1337 <dmitry.trudovoy@proton.me>
2025-06-15 19:37:29 +0200

    feat: Adds sturdy leather recipe
    Changes:
     - Title and style change
     - Ingredients change (flex styling and separates into raw and intermediate sections)
     - New Ingredient divs
     - New crafting recipe sections, with toggle

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,14 @@
 const hideTypes = [
-  ["---", "---"],
+  ["Select Ingredient", "---"],
+  ["Leather:", "---", "disabled"],
   ["Small hide", "small_hide"],
   ["Medium hide", "medium_hide"],
   ["Large hide", "large_hide"],
   ["Huge hide", "huge_hide"],
+
+  ["Sturdy Leather:", "---", "disabled"],
+  ["Large hide", "large_hide_sturdy"],
+  ["Huge hide", "huge_hide_sturdy"],
 ];
 
 const cants = {
@@ -31,6 +36,20 @@ const cants = {
     maxHidePerBarrel: 5,
     size: 'Huge',
   },
+  large_hide_sturdy: {
+    cantLiquidPerHide: 6,
+    cantLeather: 3,
+    maxHidePerBarrel: 8,
+    size: 'Large',
+    isSturdy: true,
+  },
+  huge_hide_sturdy: {
+    cantLiquidPerHide: 10,
+    cantLeather: 6,
+    maxHidePerBarrel: 5,
+    size: 'Huge',
+    isSturdy: true,
+  },
 };
 
 const select = document.querySelector("#selectHide"),
@@ -52,8 +71,33 @@ const cantSelectedHideSpan = document.querySelectorAll(".cantSelectedHideSpan"),
   cantBoraxSpan = document.querySelectorAll(".cantBoraxSpan"),
   cantPoweredBoraxSpan = document.querySelectorAll(".cantPoweredBoraxSpan");
 
+// Cooking Pot values
+const cantPotAcid = document.querySelectorAll(".cantPotAcid");
+const cantPotSulfate = document.querySelectorAll(".cantPotSulfate");
+
+const potOpsMax = document.querySelectorAll(".potOpsMax")
+const potOpsAcid = document.querySelector(".potOpsAcid")
+const potOpsSulfate = document.querySelector(".potOpsSulfate")
+const potOpsTanned = document.querySelector(".potOpsTanned")
+const potOpsLeather = document.querySelector(".potOpsLeather")
+
+// Non-shared Recipes
+const allLeather = document.querySelectorAll(".leather")
+const allSturdy = document.querySelectorAll(".sturdy")
+const leatherSwitcheroo = document.querySelector(".leather-switcheroo")
+
+// Shopping list entries
+const shoppingLeather = document.querySelectorAll(".shopping-leather")
+const shoppingSturdy = document.querySelectorAll(".shopping-sturdy")
+
+const shoppingBeeswax = document.querySelectorAll(".cantShopBeeswax")
+const shoppingSaltpeter = document.querySelectorAll(".cantShopSaltpeter")
+const shoppingChromite = document.querySelectorAll(".cantShopChromite")
+const shoppingSulfur = document.querySelectorAll(".cantShopSulfur")
+
 const allHides = document.querySelectorAll(".hide-pic")
 
+const maxUnitsPerCookingPot = 6;
 const minWaterPerBarrel = 10,
   maxWaterPerBarrel = 50;
 
@@ -69,6 +113,7 @@ for (var i = 0; i < hideTypes.length; i++) {
   var opcion = document.createElement("option");
   opcion.text = hideTypes[i][0];
   opcion.value = hideTypes[i][1];
+  opcion.disabled = hideTypes[i][2]
   select.add(opcion);
 }
 
@@ -90,6 +135,7 @@ select.addEventListener("change", (e) => {
   init();
   calculate();
   image_replace(cants[value].size)
+  toggleRecipes(cants[value].isSturdy)
 });
 
 // INPUT RANGE HIDE
@@ -148,22 +194,30 @@ function init() {
   inputRangeLeather.max = maxLeather;
   inputRangeLeather.min = leatherPerHide;
   inputRangeLeather.value = 1;
+
+  potOpsMax.forEach((e) => {
+    e.textContent = maxUnitsPerCookingPot
+  })
 }
 
 function calculate() {
   cantLimeSpan.forEach((e) => {
     e.textContent = cantSelectedHide * liquidPerHide;
   });
+
   cantBoraxSpan.forEach((e) => {
     const cantBorax = Math.ceil((cantSelectedHide * liquidPerHide) / 5) * 5;
     e.textContent = cantBorax;
   });
+
   cantPoweredBoraxSpan.forEach((e) => {
     e.textContent = Math.ceil((cantSelectedHide * liquidPerHide) / 5) * 2;
   });
+
   cantLiquidSpan.forEach((e) => {
     e.textContent = Math.ceil((cantSelectedHide * liquidPerHide) / 10) * 10;
   });
+
   cantTotalLiquidSpan.forEach((e) => {
     const taninWater =
       Math.ceil((cantSelectedHide * liquidPerHide) / 10) * 10 * 2;
@@ -177,6 +231,48 @@ function calculate() {
   cantTotalLogsSpan.forEach((e) => {
     e.textContent = Math.ceil((cantSelectedHide * liquidPerHide) / 10) * 3;
   });
+
+  if (!cants[selectedHide].isSturdy) {
+    return
+  }
+
+  // Sturdy Leather Calc
+  let beeswax = cantSelectedHide * 3
+  let sulfate = cantSelectedHide * 3
+
+  let chromite = sulfate * 2
+  let acid = sulfate * 2
+
+  let sulfur = acid * 2
+
+  // Crafting Grids
+  cantPotAcid.forEach((e) => {
+    e.textContent = acid
+  })
+
+  cantPotSulfate.forEach((e) => {
+    e.textContent = sulfate
+  })
+
+  // Pot Crafts
+  potOpsAcid.textContent = Math.max(Math.floor(acid / maxUnitsPerCookingPot), 1)
+  potOpsSulfate.textContent = Math.max(Math.floor(sulfate / maxUnitsPerCookingPot), 1)
+  potOpsTanned.textContent = Math.max(Math.floor(cantSelectedHide / maxUnitsPerCookingPot), 1)
+  potOpsLeather.textContent = Math.max(Math.floor(cantSelectedHide / maxUnitsPerCookingPot), 1)
+
+  // Shopping List
+  shoppingBeeswax.forEach((e) => {
+    e.textContent = beeswax
+  })
+  shoppingChromite.forEach((e) => {
+    e.textContent = chromite
+  })
+  shoppingSaltpeter.forEach((e) => {
+    e.textContent = acid
+  })
+  shoppingSulfur.forEach((e) => {
+    e.textContent = sulfur
+  })
 }
 
 function resetValues() {
@@ -190,6 +286,7 @@ function resetValues() {
 }
 
 // Takes in a size of the hide. I.E. 'Large' or 'Small' and replaces all hides with the correct size version
+// FIXME: Causes an error for Chromium Tanned Hides because there is no small or medium versions
 function image_replace(size) {
   allHides.forEach((e) => {
     baseSrc = e.getAttribute('data-base-src')
@@ -198,4 +295,52 @@ function image_replace(size) {
     baseTitle = e.getAttribute('data-base-title')
     e.title = `${baseTitle} (${size})`
   });
+}
+
+function toggleRecipes(isSturdy) {
+  if (isSturdy) {
+    // Leather Slider
+    let baseSrc = leatherSwitcheroo.getAttribute("data-base-src")
+    leatherSwitcheroo.src = `${baseSrc}-sturdy.png`
+    leatherSwitcheroo.title = 'Sturdy Leather'
+    
+    // Crafting Grids
+    allLeather.forEach((e) => {
+      e.style.display = 'none'
+    })
+    allSturdy.forEach((e) => {
+      e.style.display = 'block'
+    })
+
+    // Shopping List
+    shoppingLeather.forEach((e) => {
+      e.style.display = 'none'
+    })
+    shoppingSturdy.forEach((e) => {
+      e.style.display = 'flex'
+    })
+
+  } else {
+    
+    // Leather Slider
+    let baseSrc = leatherSwitcheroo.getAttribute("data-base-src")
+    leatherSwitcheroo.src = `${baseSrc}.png`
+    leatherSwitcheroo.title = 'Leather'
+      
+    // Crafting Grids
+    allLeather.forEach((e) => {
+      e.style.display = 'block'
+    })
+    allSturdy.forEach((e) => {
+      e.style.display = 'none'
+    })
+
+    // Shopping List
+    shoppingLeather.forEach((e) => {
+      e.style.display = 'flex'
+    })
+    shoppingSturdy.forEach((e) => {
+      e.style.display = 'none'
+    })
+  }
 }
