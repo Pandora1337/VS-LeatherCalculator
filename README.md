47b6aeb2c7a146751da4b77fd106772ff316797c
Pandora1337 <dmitry.trudovoy@proton.me>
2025-06-18 16:56:52 +0200

    V3 initial
    Changes:
     - Recursive recipes
     - Recipes and Items stored in JSON
     - Generated crafting grids
     - New Icons

--- README.md
+++ README.md
@@ -1,35 +1,7 @@
 # Leather Working Calculator
-A simple raw materials calculator for Leather and Sturdy Leather for video game Vintage Story
+A recursive raw materials calculator with recipes for video game Vintage Story
 
-## Quantities
-
-| Size    | qty. solvent/hide | qty. tanin/hide | qty. leather/hide |
-| ------- | ----------------- | --------------- | ----------------- |
-| small | 2L                | 2L              | 1                 |
-| medium | 4L                | 4L              | 2                 |
-| large  | 6L                | 6L              | 3                 |
-| huge  | 10L               | 10L             | 5                 |
-
-- 50L x barrel
-- 5L diluited borax = 5L water + 2 powered borax
-- 1L limewater = 1L + 1 lime
-- 10L weak tanin = 1 log x 10L water
-- 10L strong tanin = 1 log x 10L tanin débil (1 log x 10L water + 1 log )
-
-## Cantidad minima en barril
-
-| Size    | qty. hide | qty. solvent | weak tanin (water x log)       | strong tanin (w.t. x log)               | qty. leather |
-| ------- | --------- | ------------ | ------------------------------ | --------------------------------------- | ------------ |
-| small | 5         | 10L          | 10L x 1 log                    | (10L x 1 log) x 1 log                   | 5            |
-| medium | 2         | 8L           | 10L x 1 log - surplus 2L s.t.  | (10L x 1 log) x 1 log - surplus 2L s.t. | 4            |
-| large  | 1         | 6L           | 10L x 1 logs - surplus 4L w.t. | (10L x 1 log) x 1 log - surplus 4L s.t. | 3            |
-| huge  | 1         | 10L          | 10L x 1 log                    | (10L x 1 log) x 1 log                   | 5            |
-
-## Cantidad maxima en barril
-
-| Size    | qty. hide | qty.  solvent | weak tanin (water x logs)      | strong tanin (w.t. x logs)                | qty. leather |
-| ------- | --------- | ------------- | ------------------------------ | ----------------------------------------- | ------------ |
-| small | 25        | 50L           | 50L x 5 logs                   | (50L x 5 logs) x 5 logs                   | 25           |
-| medium | 12        | 48L           | 50L x 5 logs - surplus 2L w.t. | (50L x 5 logs) x 5 logs - surplus 2L s.t. | 24           |
-| large  | 8         | 48L           | 50L x 5 logs - surplus 2L w.t. | (50L x 5 logs) x 5 logs - surplus 2L s.t. | 24           |
-| huge  | 5         | 50L           | 50L x 5 logs                   | (50L x 5 logs) x 5 logs                   | 25           |
+## Features
+ - Flexible: Any recipe is supported! Simply write down the recipe and items in JSON
+ - Recursive recipes: the crafting chain will be generated automatically from recipes with correct amounts!
+ - Ingredients shopping list: an easy to read list of required items - Raw or Intermediate!
---
+++ script.js
@@ -0,0 +1,187 @@
+let recipes = null;
+let items = null;
+
+// load recipes
+(async () => {
+    recipes = await loadJSON("recipes.json")
+    items = await loadJSON("items.json")
+    console.log("Recipes and Items loaded")
+    // recursiveCrafting("leather-sturdy-huge", 32)
+    // addShoppingIngredient("leather", 1)
+})()
+
+async function loadJSON(file) {
+    return fetch(file)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json()
+        })
+}
+
+const grids = document.querySelector("#crafting-grids")
+function generateCraftingGrid(value) {
+    grids.innerHTML = ""
+    // recursiveCrafting("leather-sturdy-large")
+    recursiveCrafting(value, 1)
+    shop.innerHTML = ""
+}
+
+function generateCrafting(recipeId, amount = 1) {
+    const recipe = recipes[recipeId]
+    const recipeItem = items[recipe.item]
+
+    // Container
+    let div = document.createElement('div')
+    div.classList = "tableContainer"
+
+    // Title
+    let title = document.createElement("h4")
+    title.textContent = recipeItem.name
+    div.appendChild(title)
+
+    // Table and Row
+    let table = document.createElement("table");
+    div.appendChild(table);
+
+    let tr = document.createElement('tr');
+    table.appendChild(tr)
+
+    // Crafting Station
+    if (recipe.station.item) {
+        tr.appendChild(getCraftingItem(items[recipe.station.item]))
+        tr.appendChild(getCraftingSign(":"))
+    }
+
+    // Ingredients
+    let hasLiquid = false
+    let maxCount = 0
+    for (let i = 0; i < recipe.ingredients.length; i++) {
+        const ingredient = recipe.ingredients[i]
+        const count = ingredient.count * amount
+
+        if (ingredient.option) {
+            const ingredientOpt = items[ingredient.option.item]
+            const countOpt = ingredient.option.count * amount
+
+            tr.appendChild(getCraftingItem(ingredientOpt, countOpt))
+            tr.appendChild(getCraftingSign("OR"))
+            
+            if (ingredientOpt.liquid) {
+                hasLiquid = true
+            }
+
+            if (countOpt > maxCount){
+                maxCount = countOpt
+            }
+        }
+
+        tr.appendChild(getCraftingItem(items[ingredient.item], count))
+        tr.appendChild(getCraftingSign(i == recipe.ingredients.length - 1 ? "=" : "+"))
+        
+        if (items[ingredient.item].liquid) {
+            hasLiquid = true
+        }
+
+        if (count > maxCount){
+            maxCount = count
+        }
+    }
+
+    // Result
+    tr.appendChild(getCraftingItem(recipeItem, recipe.count * amount))
+
+    // Number of crafts
+    if (recipe.station.portionSize) {
+        let span = document.createElement('span')
+        span.textContent = `Number of crafts: ${Math.ceil(maxCount / recipe.station.portionSize)}` +
+            ` (Max ${recipe.station.portionSize}${hasLiquid ? "L" : " items"} at a time)`
+        div.appendChild(span)
+    }
+
+    // grids.appendChild(div);
+    grids.insertBefore(div, grids.firstChild);
+}
+
+function getCraftingItem(item, count = 0) {
+    let td = document.createElement('td');
+    if (!item) {
+        console.log("Not an item: " + item)
+        return td
+    }
+
+    td.innerHTML = `
+        <img src="./assets/img/${item.id}.png" title="${item.name}">
+    `
+
+    if (count != 0) {
+        td.innerHTML += `
+            <br><span">${count}${item.liquid ? "L" : ""}</span></br>
+        `
+    }
+    return td
+}
+
+function getCraftingSign(sign) {
+    let td = document.createElement('td');
+    td.textContent = sign
+    return td
+}
+
+function recursiveCrafting(rootRecipe, amount) {
+    var seen = new Set()
+    function recurse(targetRecipe, targetAmount) {
+        let recipe = recipes[targetRecipe]
+        if (seen.has(targetRecipe) || !recipe) 
+            return
+
+        // Number of crafts
+        const num = Math.ceil(targetAmount / recipe.count)
+        seen.add(recipe.item)
+
+        generateCrafting(targetRecipe, num)
+
+        // Ingredients Total
+        let dict = {}
+        for (let i = 0; i < recipe.ingredients.length; i++) {
+            const ingredient = recipe.ingredients[i];
+
+            dict[ingredient.item] = (dict[ingredient.item] || 0) + num * ingredient.count
+
+            const ingredientOpt = ingredient.option
+            if (ingredientOpt){
+                dict[ingredientOpt.item] = (dict[ingredientOpt.item] || 0) + num * ingredientOpt.count;
+                recurse(ingredientOpt.item, num * ingredientOpt.count)
+            }
+        }
+        // TODO: 1 input into 2 recipe is broken
+        for (let key in dict) {
+            let count = dict[key]
+            recurse(key, count)
+            addShoppingIngredient(key, count)
+        }
+    }
+    recurse(rootRecipe, amount)
+}
+
+const shop = document.querySelector(".item-containers")
+function addShoppingIngredient(itemId, amount, category) {
+    let item = items[itemId]
+
+    let div = document.createElement('div')
+    div.classList = "item"
+
+    // Title
+    div.innerHTML += `
+        <br>${item.name}</br>
+    `
+    // Icon
+    div.appendChild(getCraftingItem(item))
+
+    // Count
+    div.innerHTML += `
+        <br><span">${amount}${item.liquid ? "L" : ""}</span></br>
+    `
+    shop.appendChild(div)
+}
